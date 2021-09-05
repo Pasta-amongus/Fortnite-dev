96b1425f44fa9ed36fbafed8f5d671ac72f9371a
Pasta-amongus <88669889+Pasta-amongus@users.noreply.github.com>
2021-09-05 12:52:26 +0000

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=
+REM BFCPEICON=
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
+cd %CD%\SimonFN
+IF EXIST node_modules (
+color 05
+echo Du hast die Node Module bereits installiert %CD%\node_modules Felder
+pause
+) ELSE (
+echo[
+echo Installiert node_modules in %CD%
+echo[
+npm i
+pause
